f4a85981b87f8e159ae5c7a5944fbb0a7f3aa0bc
ZeroBugZone417 <dinethsudarshana417@gmail.com>
2026-01-16 09:41:47 +0530

    Delete public directory

--- public/script.js
+++
@@ -1,8 +0,0 @@
-function download() {
-  const url = document.getElementById('ytUrl').value;
-  if (!url) {
-    document.getElementById('msg').innerText = 'Paste a YouTube link';
-    return;
-  }
-  window.location.href = `/ytmp3?url=${encodeURIComponent(url)}`;
-}
